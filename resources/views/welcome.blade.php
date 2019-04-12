a62836a335ad8f45660d9637e50dfcd33dfc0d68
jennifer <1793880774@qq.com>
2019-04-12 07:06:39 +0000

    修改welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel  weixin
                 </div>
 
                 <div class="links">
